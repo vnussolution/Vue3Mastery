967b636969b604436a67605f2714bea12c8ca9aa
TRUC NGUYEN <trucnguyen@TRUCs-MacBook-Pro.local>
2020-09-22 08:02:00 -0700

    lession 7 , methods

--- main.js
+++ main.js
@@ -8,9 +8,25 @@ const app = Vue.createApp({
             onSale: false,
             details:['50% cotton','30% wool', '20% polyester'],
             items:[
-                {id:123,color:'green'},
-                {id:234,color:'blue'},
-            ]
+                {id:123,color:'green',image:'./assets/images/socks_green.jpg'},
+                {id:234,color:'blue', image:'./assets/images/socks_blue.jpg'},
+            ],
+            cart:0,
+            inStock:false
+
+        }
+    },
+    methods:{
+        addToCart(){
+            this.cart += 1;
+        },
+        removeFromCart(){
+            if(this.cart >0)
+            this.cart -=1;
+        },
+        updateImage(image){
+            console.log('---',image);
+            this.image = image;
         }
     }
 });
